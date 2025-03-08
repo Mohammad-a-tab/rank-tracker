b96c123d64f7e4046280af53b0c1e97e3568b4e1
mohammad-a-tab <mohammad.aali.tab@gmail.com>
2025-03-08 11:53:19 +0330

    chore: enhanced code base

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -4,50 +4,45 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Models\User;
 use App\Traits\JsonResponseTrait;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use App\Http\Requests\Api\V1\Auth\AuthLoginRequest;
 
 class AuthController extends Controller
 {
     use JsonResponseTrait;
 
-    public function login(AuthLoginRequest $request)
+    /**
+     * @param AuthLoginRequest $request
+     * @return JsonResponse
+     */
+    public function login(AuthLoginRequest $request): JsonResponse
     {
         $user = User::query()
             ->where('username', $request->username)
             ->first();
 
         if (!$user) {
-            return $this->errorResponse(
-                'کاربر یافت نشد',
-                404
-            );
+            return $this->errorResponse(__('messages.user_not_found'), Response::HTTP_NOT_FOUND);
         }
 
         if(!Hash::check($request->password, $user->password)) {
-            return $this->errorResponse(
-                'نام کاربری یا پسورد اشتباه است!',
-                401
-            );
+            return $this->errorResponse(__('messages.login_failed'), Response::HTTP_UNAUTHORIZED);
         }
 
         $token = $user->createToken('API')->plainTextToken;
 
-        return $this->successResponse(
-            'ورود با موفقیت!',
-            [
-                'token'     => $token
-            ]
-        );
+        return $this->successResponse(__('messages.login_successfully'), ['token' => $token]);
     }
 
-    public function logout()
+    public function logout(): JsonResponse
     {
         $loggedInUser = auth('sanctum')->user();
 
         $loggedInUser->currentAccessToken()->delete();
 
-        return $this->successResponse('شما با موفقیت خارج شدید.');
+        return $this->successResponse(__('messages.logout_successfully'));
     }
 }
